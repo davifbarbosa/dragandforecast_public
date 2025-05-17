e6fe700b69fa0db149eb52bdf4d7fb81bd6803a2
Ajmal-hassan <ajmalhassan651@gmail.com>
2025-05-17 12:29:48 +0500

    bootstrap issue fixed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import  "./bootstrap/js/bootstrap"
 import  "./glightbox/js/glightbox.min"
 import  "./imagesloaded.pkgd.min"
 import  "./isotope-layout/isotope.pkgd.min"
