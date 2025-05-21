4f7874a8f8a4000997eaf4fc3c30388bc809d714
Ajmal-hassan <ajmalhassan651@gmail.com>
2025-05-21 23:11:46 +0500

    table loading performance updated

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import  "./glightbox/js/glightbox.min"
-import  "./imagesloaded.pkgd.min"
-import  "./isotope-layout/isotope.pkgd.min"
-import  "./script"
