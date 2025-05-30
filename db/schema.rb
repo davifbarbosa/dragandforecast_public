04ee7e7d6dfb0689633690be43d027ab7b894f20
davifbarbosa <79879015+davifbarbosa@users.noreply.github.com>
2025-05-30 14:00:34 -0300

    first public version

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_16_184440) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_21_051909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actuals", force: :cascade do |t|
+    t.jsonb "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_actuals_on_user_id"
+  end
 
   create_table "csv_files", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -21,6 +29,42 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_16_184440) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_csv_files_on_user_id"
+  end
+
+  create_table "forecast_row_backups", force: :cascade do |t|
+    t.jsonb "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "forecast_row_id", null: false
+    t.index ["forecast_row_id"], name: "index_forecast_row_backups_on_forecast_row_id"
+    t.index ["user_id"], name: "index_forecast_row_backups_on_user_id"
+  end
+
+  create_table "forecast_rows", force: :cascade do |t|
+    t.jsonb "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_forecast_rows_on_user_id"
+  end
+
+  create_table "plan_purchase_requests", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "subscription_plan_id", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "company"
+    t.string "function"
+    t.string "email"
+    t.string "country"
+    t.string "state"
+    t.boolean "agreed_to_terms"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subscription_plan_id"], name: "index_plan_purchase_requests_on_subscription_plan_id"
+    t.index ["user_id"], name: "index_plan_purchase_requests_on_user_id"
   end
 
   create_table "subscription_plans", force: :cascade do |t|
@@ -42,11 +86,19 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_16_184440) do
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
     t.bigint "subscription_plan_id"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["subscription_plan_id"], name: "index_users_on_subscription_plan_id"
   end
 
+  add_foreign_key "actuals", "users"
   add_foreign_key "csv_files", "users"
+  add_foreign_key "forecast_row_backups", "forecast_rows"
+  add_foreign_key "forecast_row_backups", "users"
+  add_foreign_key "forecast_rows", "users"
+  add_foreign_key "plan_purchase_requests", "subscription_plans"
+  add_foreign_key "plan_purchase_requests", "users"
   add_foreign_key "users", "subscription_plans"
 end
